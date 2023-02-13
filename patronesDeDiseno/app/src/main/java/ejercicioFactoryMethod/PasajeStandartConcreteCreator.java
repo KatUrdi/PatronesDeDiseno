055288eb9c3ca36bbe818070ae1b81a868b55240
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-13 11:33:26 -0400

    Update PasajeStandartConcreteCreator.java

--- patronesDeDiseno/app/src/main/java/ejercicioFactoryMethod/PasajeStandartConcreteCreator.java
+++ patronesDeDiseno/app/src/main/java/ejercicioFactoryMethod/PasajeStandartConcreteCreator.java
@@ -1,15 +1,15 @@
 package factoryMethod.ejercicioFactoryMethod;
 
-public class PasajeStandartConcreteCreator extends CreatorPasaje{
+public class PasajeStandartConcreteCreator extends PasajeCreator{
 
     @Override
     public Pasaje1 createPasaje(Pasajero pasajero, String numeroAsiento) {
-        PasajeStandard pasaje = new PasajeStandard();
-        pasaje.setAvion(new Avion("MOPAR", "1 ton", "AK-45", 100));
-        pasaje.setCostoPasaje("100 US");
-        pasaje.setOrigen(new Lugar("Japon", "Tokyo", "Aeropueto Internacional de Tokyo"));
-        pasaje.setDestino(new Lugar("Irlanda", "Dublin", "Aeropuerto Internacional de Dublin"));
-        pasaje.setNumeroVuelo("123AJ");
+        PasajeStandard pasaje = new PasajeStandart();
+        pasaje.setAvion(new Avion("Boeing", "1.5 ton", "747", 120));
+        pasaje.setCostoEspecial("45 US");
+        pasaje.setOrigen(new Lugar("España", "Madrid", "Aeropueto Internacional de Madrid"));
+        pasaje.setDestino(new Lugar("Bolivia", "La Paz", "Aeropuerto Internacional del Alto"));
+        pasaje.setNumeroVuelo("680AB");
         pasaje.setNumeroAsiento(numeroAsiento);
         pasaje.setPasajero(pasajero);
         return pasaje;
