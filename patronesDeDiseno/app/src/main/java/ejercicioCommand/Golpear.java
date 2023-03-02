2674600e491e7236aef4a54b53b852cc0e278081
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-02 14:56:27 -0400

    Create Golpear.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioCommand/Golpear.java
@@ -0,0 +1,14 @@
+package ejercicioCommand;
+
+public class golpear implements interfazComandos {
+    private persona ty;
+
+    public golpear(persona ty) {
+        this.ty = ty;
+    }
+
+    @Override
+    public void execute() {
+        this.ty.cover();
+    }
+}
