28f92a2cfa0e6072d8db448e46590016873d49ae
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-02 14:50:37 -0400

    Create Correr.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioCommand/Correr.java
@@ -0,0 +1,14 @@
+package ejercicioCommand;
+
+public class correr implements interfazComandos {
+    private persona ty;
+
+    public correr(persona ty) {
+        this.ty = ty;
+    }
+
+    @Override
+    public void execute() {
+        this.ty.cover();
+    }
+}
