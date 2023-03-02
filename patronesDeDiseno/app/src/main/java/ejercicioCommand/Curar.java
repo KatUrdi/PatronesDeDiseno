ee310bbb5420af013ad48ff8e70d452040f79177
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-02 14:55:41 -0400

    Create Curar.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioCommand/Curar.java
@@ -0,0 +1,14 @@
+package ejercicioCommand;
+
+public class curar implements interfazComandos {
+    private persona ty;
+
+    public  curar(persona ty) {
+        this.ty = ty;
+    }
+
+    @Override
+    public void execute() {
+        this.ty.cover();
+    }
+}
