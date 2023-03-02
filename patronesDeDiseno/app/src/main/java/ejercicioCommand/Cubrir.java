0f252c405984c8a746d84a5997f061726ebbfc3c
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-02 14:55:06 -0400

    Create Cubrir.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioCommand/Cubrir.java
@@ -0,0 +1,14 @@
+package ejercicioCommand;
+
+public class cubrir implements interfazComandos {
+    private persona ty;
+
+    public cubrir(persona ty) {
+        this.ty = ty;
+    }
+
+    @Override
+    public void execute() {
+        this.ty.cover();
+    }
+}
