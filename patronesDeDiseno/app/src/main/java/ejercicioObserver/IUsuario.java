4add28454279893953321e84e30fca21c94c2dff
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-03 11:55:47 -0400

    Create IUsuario.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioObserver/IUsuario.java
@@ -0,0 +1,10 @@
+package ejercicioObserver;
+
+import java.util.List;
+
+public interface IUsuario {
+    void update(String propaganda, String categoria);
+    void addCategoria(String categoria);
+    List<String> getCategorias();
+    void quitarCategoria(String categoria);
+}
