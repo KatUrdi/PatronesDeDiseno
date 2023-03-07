230a71f350540489debbca6e0d7bd857d3b49ead
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-07 08:49:06 -0400

    Create IArea.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioVisitor/IArea.java
@@ -0,0 +1,8 @@
+package ejercicioVisitor;
+
+public interface IArea {
+    void darVisa(ILibreCambista libreCambista);
+    double cambiarABol(double monto);
+    double cambiarADol(double monto);
+    double cambiarAEu(double monto);
+}
