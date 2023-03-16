6c72ced41c6e02ce16482d270ba53a138593ab1c
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-16 11:27:52 -0400

    Create Windowsx128.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioBridge/sinBridge/Windowsx128.java
@@ -0,0 +1,10 @@
+package ejercicioBridge.sinBridge;
+
+public class Windowsx128 extends Windows {
+
+    public Windowsx128() {
+        this.setX128();
+        this.setVersion("7");
+    }
+    
+}
