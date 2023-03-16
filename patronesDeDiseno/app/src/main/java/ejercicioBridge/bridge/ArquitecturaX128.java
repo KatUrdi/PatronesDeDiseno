48ba81528ed3c59c524d9243ef5d1f6ccc05339b
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-16 11:03:36 -0400

    Create ArquitecturaX128.java

---
+++ patronesDeDiseno/app/src/main/java/ejercicioBridge/bridge/ArquitecturaX128.java
@@ -0,0 +1,11 @@
+package ejercicioBridge.bridge;
+
+public class ArquitecturaX128 implements Arquitectura {
+    private String tipo = "X128";
+
+    @Override
+    public String getTipo() {
+        return tipo;
+    }
+
+}
